5baed467a9e29f0a762b9198c8f31dd36cc21d2a
1337-Defender <crdsouza2004@gmail.com>
2025-05-01 19:35:49 +0400

    Redirect to /challenges on sign up

--- mahe_ctf/app/actions.ts
+++ mahe_ctf/app/actions.ts
@@ -56,7 +56,7 @@ export const signInAction = async (formData: FormData) => {
     return encodedRedirect("error", "/sign-in", error.message);
   }
 
-  return redirect("/protected");
+  return redirect("/challenges");
 };
 
 export const forgotPasswordAction = async (formData: FormData) => {
