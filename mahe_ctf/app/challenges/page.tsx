4cc2fe1ffa7187bec3c7a47404827d08aa49f7a9
1337-Defender <crdsouza2004@gmail.com>
2025-05-01 21:57:01 +0400

    Added challenges dashboard page.

--- mahe_ctf/app/challenges/page.tsx
+++ mahe_ctf/app/challenges/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {TeamStats} from "@/components/team-stats";
+import {ChallengeCategories} from "@/components/challenge-categories";
 
 const ChallengesPage = async () => {
     return (
-        <div>
-            <h1>Challenges</h1>
+        <div className="container py-6 space-y-8">
+          <h1 className="text-3xl">Adventure Dashboard</h1>
+          <TeamStats />
+          <div className="space-y-4">
+            <h2 className="text-2xl">Quest Categories</h2>
+            <ChallengeCategories />
+          </div>
         </div>
     );
 };
