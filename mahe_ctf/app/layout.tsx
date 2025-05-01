d9b366bb834d97e50dd1b4773ede33256e9fbb25
1337-Defender <crdsouza2004@gmail.com>
2025-05-01 19:05:00 +0400

    Cleaned up supabase starter elements.

--- mahe_ctf/app/layout.tsx
+++ mahe_ctf/app/layout.tsx
@@ -1,7 +1,6 @@
 import DeployButton from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
@@ -54,20 +53,9 @@ export default function RootLayout({
                 {children}
               </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-                <p>
-                  Powered by{" "}
-                  <a
-                    href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    Supabase
-                  </a>
-                </p>
-                <ThemeSwitcher />
-              </footer>
+              {/*<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">*/}
+              {/*  <h3>Hosted by MAHE, Dubai</h3>*/}
+              {/*</footer>*/}
             </div>
           </main>
         </ThemeProvider>
