82bbfc573aea662f60dc0ff5e3f24519bd6fc57d
1337-Defender <crdsouza2004@gmail.com>
2025-05-03 16:43:33 +0400

    Added server action to get challenges from supabase db

--- mahe_ctf/lib/actions.ts
+++ mahe_ctf/lib/actions.ts
@@ -1,4 +1,5 @@
 "use server"
+import { createClient } from "@/utils/supabase/server";
 
 // This is a mock implementation of flag verification
 // In a real application, this would verify against a database
@@ -11,4 +12,23 @@ export async function verifyFlag(challengeId: string, flag: string): Promise<{ s
   const isCorrect = flag.toLowerCase().includes("correct")
 
   return { success: isCorrect }
+}
+
+export async function getChallenges(category: string) {
+  const supabase = await createClient();
+  // let { data, error } = await supabase
+  //   .from("challenge")
+  //   .select("id, name, description, category, difficulty, points, challenge_link(name, url)")
+  //   .eq("category", category)
+  //   .order("id", { ascending: true });
+  //
+  // // Temporarily setting challenges to unsolved
+  // data = data?.map(challenge => ({ ...challenge, solved: false })) || []
+  let { data, error } = await supabase.rpc("get_challenges_with_details", { tid: 3, cat: category })
+  // console.log(data[0].links)
+  // console.log(typeof data[0].links)
+  // data = data?.map(challenge => {
+  //   challenge.links = JSON.parse(challenge.links);
+  // }) || []
+  return data;
 }
